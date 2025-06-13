cefa3b40d83e19bd70d1809625d98f1a81236c65
Thien Truong <xayda.ln@gmail.com>
2025-06-13 21:38:13 +0700

    Add search function

--- index.js
+++ index.js
@@ -1,100 +1,123 @@
-    let token;
-    document.addEventListener("DOMContentLoaded", function () {
-      initialApp();
-    });
+let token;
+let searchTimeout;
+document.addEventListener("DOMContentLoaded", function () {
+  initialApp();
+  setupSearchListener();
+});
 
-    async function initialApp() {
-      token = await getSpotifyToken();
-      if (token) {
-        const response = await getPopularTrack();
+async function initialApp() {
+  token = await getSpotifyToken();
+  if (token) {
+    const response = await getPopularTrack("Central Cee");
+    displayTrack(response.tracks.items);
+  }
+}
+
+function setupSearchListener() {
+  const inputSearch = document.getElementById("input-search");
+  inputSearch.addEventListener("input", (event) => {
+
+    const querry = event.target.value.trim();
+    clearTimeout(searchTimeout);
+    //debounce : them tg delay    
+    searchTimeout = setTimeout(async () => {
+      if (querry) {
+        const response = await getPopularTrack(querry);
+        resetTrack();
         displayTrack(response.tracks.items);
       }
-    }
+    }, 2000);
+  });
+}
 
-    async function displayTrack(data) {
-      console.log(data);
-      data.forEach((item) => {
-        // tạo biến thay thếthế
-        console.log(item.id);
-        const imageUrl = item.album.images[0].url;
-        const name = item.name;
-        const artistName = item.artists.map((item) => item.name).join(" - ");
-        // tạo ra thẻ div
-        const element = document.createElement("div");
-        // gắn class cho thẻ div
-        element.className = "track-card";
-        // gắn nội dung cho thẻ div
-        element.innerHTML = `<div class="track-card-container">
+function resetTrack() {
+  const trackSection = document.getElementById("track-section");
+  trackSection.innerHTML = "";
+}
+
+async function displayTrack(data) {
+  console.log(data);
+  data.forEach((item) => {
+    // tạo biến thay thếthế
+    const imageUrl = item.album.images[0].url;
+    const name = item.name;
+    const artistName = item.artists.map((item) => item.name).join(" - ");
+    // tạo ra thẻ div
+    const element = document.createElement("div");
+    // gắn class cho thẻ div
+    element.className = "track-card";
+    // gắn nội dung cho thẻ div
+    element.innerHTML = `<div class="track-card-container">
           <img src="${imageUrl}">
           <h3>${name}</h3>
           <p>${truncateText(artistName, 30)}</p>
         </div>`;
-        // thêm event listener để phát nhạc
-        element.addEventListener("click", () => {
-          playTrack(item.id,name,artistName);
-        });
-        // gắn thẻ div đó vào track-sectionsection
-        const trackSection = document.getElementById("track-section");
-        trackSection.appendChild(element);
-      });
-    }
+    // thêm event listener để phát nhạc
+    element.addEventListener("click", () => {
+      playTrack(item.id, name, artistName);
+    });
+    // gắn thẻ div đó vào track-sectionsection
+    const trackSection = document.getElementById("track-section");
+    trackSection.appendChild(element);
+  });
+}
 
-    function playTrack(id,name,artistName) {
-      const iframe = document.getElementById("iframe");
-      iframe.src = `https://open.spotify.com/embed/track/${id}?utm_source=generator&theme=0`;
-      const modals = document.getElementById("modals");
-      modals.style.display = "block";
-      const modalsName = document.getElementById("modals-name");
-      modalsName.innerHTML = name;
-    }
+function playTrack(id, name, artistName) {
+  const iframe = document.getElementById("iframe");
+  iframe.src = `https://open.spotify.com/embed/track/${id}?utm_source=generator&theme=0`;
+  const modals = document.getElementById("modals");
+  modals.style.display = "block";
+  const modalsName = document.getElementById("modals-name");
+  modalsName.innerHTML = name;
+}
 
-    function handleClose() {
-      const modals = document.getElementById("modals");
-      modals.style.display = "none";
-    }
+function handleClose() {
+  const modals = document.getElementById("modals");
+  modals.style.display = "none";
+}
 
-    function truncateText(text, number) {
-      return text.length > number ? text.slice(0,number) + "..." : text;
-    }
+function truncateText(text, number) {
+  return text.length > number ? text.slice(0, number) + "..." : text;
+}
 
-    async function getPopularTrack() {
-      try {
-        const response = await axios.get("https://api.spotify.com/v1/search", {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-          params: {
-            q: "Central Cee",
-            type: "track",
-            market: "VN",
-            limit: 30,
-          },
-        });
-        return response.data;
-      } catch (error) {
-        console.log(error);
+async function getPopularTrack(querry = "Central Cee") {
+  try {
+    const response = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        q: querry,
+        type: "track",
+        market: "VN",
+        limit: 30,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getSpotifyToken() {
+  try {
+    const credentials = btoa(
+      `${SPOTIFY_CONFIG.CLIENT_ID}:${SPOTIFY_CONFIG.CLIENT_SECRET}`
+    );
+    const response = await axios.post(
+      "https://accounts.spotify.com/api/token",
+      "grant_type=client_credentials",
+      {
+        headers: {
+          Authorization: `Basic ${credentials}`,
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
       }
-    }
+    );
 
-    async function getSpotifyToken() {
-      try {
-        const credentials = btoa(
-          `${SPOTIFY_CONFIG.CLIENT_ID}:${SPOTIFY_CONFIG.CLIENT_SECRET}`
-        );
-        const response = await axios.post(
-          "https://accounts.spotify.com/api/token",
-          "grant_type=client_credentials",
-          {
-            headers: {
-              Authorization: `Basic ${credentials}`,
-              "Content-Type": "application/x-www-form-urlencoded",
-            },
-          }
-        );
-
-        return response.data.access_token;
-      } catch (error) {
-        console.error("Error getting token:", error);
-        return null;
-      }
-    }
+    return response.data.access_token;
+  } catch (error) {
+    console.error("Error getting token:", error);
+    return null;
+  }
+}
